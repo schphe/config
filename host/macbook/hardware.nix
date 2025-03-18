141573f033509d343c73f80af11225f42fb219ff
schphe <git@emin.sh>
2025-03-18 18:38:11 -0500

    typo

--- host/macbook/hardware.nix
+++ host/macbook/hardware.nix
@@ -39,7 +39,7 @@
         btrfs subvolume delete "$1"
       }
 
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -time +30); do
+      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
         delete_subvolume_recursively "$i"
       done
 
