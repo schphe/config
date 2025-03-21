3081bc3f838828a93374a3479d91c53c9504a834
schphe <git@emin.sh>
2025-03-20 22:11:26 -0500

    nix helper

---
+++ conf/nixhelp/default.nix
@@ -0,0 +1,12 @@
+{...}: {
+    programs.nh = {
+        enable = true;
+
+        clean = {
+            enable = true;
+            extraArgs = "--keep-since 4d --keep 3";
+        };
+
+        flake = "/pers/conf";
+    };
+}
