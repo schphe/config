ed3bf4cc0d4eb896e432fc0729803e80f552dc9b
schphe <git@emin.sh>
2025-03-25 03:49:19 -0500

    vscode nix plugin

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -1,12 +1,21 @@
-{pkgs, ...}: {
+{pkgs, utilities, ...}: {
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
 
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
         rust-lang.rust-analyzer
       ];
+
+      userSettings = utilities.flattenAttrs [] {
+        window = {
+          customMenuBarAltFocus = false;
+          menuBarVisibility = "toggle";
+          titleBarStyle = "native";
+        };
+      };
     };
   };
 
