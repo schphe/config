f2c71ded1e1f999e56585dca7cc2d6827335d044
schphe <git@emin.sh>
2026-09-22 18:01:25 -0500

    switch to librepods fork

--- feats/librepods/default.nix
+++ feats/librepods/default.nix
@@ -1,26 +1,46 @@
 {
-  flake.nixosModules.librepods = _: {
-    programs.librepods.enable = true;
+  flake.nixosModules.librepods =
+    { pkgs, ... }:
+    {
+      nixpkgs.overlays = [
+        (_: prev: {
+          librepods = prev.librepods.overrideAttrs (old: {
+            version = "0-unstable-2026-09-22";
 
-    users.users.schphe.extraGroups = [ "librepods" ];
+            src = pkgs.fetchFromGitHub {
+              owner = "harveywuk";
+              repo = "librepods";
+              rev = "4ed49df0b301ac3e6fba9c81dfbbb6726cc52201";
+              hash = "sha256-Ygoqz5lnGMwZkys+Q4c4pyAUI0llvpGZ/ij5E91CkAg=";
+            };
 
-    home-manager.users.schphe.systemd.user.services.librepods = {
-      Unit = {
-        Description = "LibrePods AirPods daemon";
-        PartOf = [ "graphical-session.target" ];
-        After = [
-          "graphical-session.target"
-          "noctalia.service"
-        ];
-        StartLimitIntervalSec = 60;
-        StartLimitBurst = 3;
+            sourceRoot = "source";
+            buildInputs = old.buildInputs ++ [ pkgs.qt6.qtdeclarative ];
+          });
+        })
+      ];
+
+      programs.librepods.enable = true;
+
+      users.users.schphe.extraGroups = [ "librepods" ];
+
+      home-manager.users.schphe.systemd.user.services.librepods = {
+        Unit = {
+          Description = "LibrePods AirPods daemon";
+          PartOf = [ "graphical-session.target" ];
+          After = [
+            "graphical-session.target"
+            "noctalia.service"
+          ];
+          StartLimitIntervalSec = 60;
+          StartLimitBurst = 3;
+        };
+        Service = {
+          ExecStart = "/run/wrappers/bin/librepods";
+          Restart = "on-failure";
+          RestartSec = 3;
+        };
+        Install.WantedBy = [ "graphical-session.target" ];
       };
-      Service = {
-        ExecStart = "/run/wrappers/bin/librepods";
-        Restart = "on-failure";
-        RestartSec = 3;
-      };
-      Install.WantedBy = [ "graphical-session.target" ];
     };
-  };
 }
